a0c64a19bb8d517d1b57e51942bf9d0e8e327e7e
Sean DeBarr <setdebarr@gmail.com>
2020-05-25 16:20:38 -0400

    fixed critical bug in setup script

--- Scripts/Initial_Setup.sh
+++ Scripts/Initial_Setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+sudo updatedb
 working_dir=$(locate -b rover_2020)
 
 echo "Installing ROS 2"
-sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo apt update && sudo apt install curl gnupg2 lsb-release -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 sudo apt update
-sudo apt install ros-eloquent-desktop
-sudo apt install python3-argcomplete
+sudo apt install ros-eloquent-desktop -y
+sudo apt install python3-argcomplete -y
 source /opt/ros/eloquent/setup.bash
 echo "source /opt/ros/eloquent/setup.bash" >> ~/.bashrc
 echo "export ROS_DOMAIN_ID=1" >> ~/.bashrc
-sudo apt install python3-colcon-common-extensions
+sudo apt install python3-colcon-common-extensions -y
 printenv | grep -i ROS
 sleep 2
 
